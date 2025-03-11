f6c57bb45d52c5f37423f8b4cabe117c92bb06d1
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-03-11 19:13:19 +0545

    Files added

--- src/com/dsa/QuestionsOnBinarySearch_5.java
+++ src/com/dsa/QuestionsOnBinarySearch_5.java
@@ -1,4 +1,5 @@
 package com.dsa;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class QuestionsOnBinarySearch_5 {
@@ -45,19 +46,55 @@ public class QuestionsOnBinarySearch_5 {
         }
         return arr[start % arr.length];
     }
+    public static int[] findNumber(int[] arr,int element){
+        int[] ans={-1,-1};
+        int start=searchNumber(arr,element,true);
+        int end=searchNumber(arr,element,false);
+        ans[0]=start;
+        ans[1]=end;
+        return ans;
+    }
+    public static int searchNumber(int[] arr,int element,boolean isFirstNumber){
+        int start=0;
+        int end=arr.length-1;
+        int ans=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]<element){
+                start=mid+1;
+            }else if(arr[mid]>element){
+                end=mid-1;
+            }else{
+                ans=mid;
+                if(isFirstNumber){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         // 1. Ceiling number
-        int []arr={1,2,3,4,5,16,17};
+        int []arr={1,2,3,3,4,5,16,17};
       //  System.out.println("Enter the item : ");
       //  int item=sc.nextInt();
        // System.out.println(CeilingNumber(arr,item));
          // 2. Floor number
       //  System.out.println(FloorNumber(arr,item));
         // 3. Return the character just greater than the given character
-        char[] arr1={'a','b','c','f','j'};
-        char element='a';
-        System.out.println(greaterElement(arr1,element));
+      //  char[] arr1={'a','b','c','f','j'};
+      //  char element='a';
+      //  System.out.println(greaterElement(arr1,element));
+        // 4. Find the first and last position of a given number in an array
+        System.out.println("Enter the element : ");
+        int element=sc.nextInt();
+        System.out.println(Arrays.toString(findNumber(arr,element)));
+
+
+
 
 
 
