78c1c65502401277a2afb66c5757b59455018be1
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-03-21 20:54:18 +0545

    Files added

--- src/com/dsa/CyclicSortQuestions_11.java
+++ src/com/dsa/CyclicSortQuestions_11.java
@@ -1,5 +1,8 @@
 package com.dsa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CyclicSortQuestions_11 {
     public static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
@@ -26,8 +29,28 @@ public class CyclicSortQuestions_11 {
         // case 2 : if the missing number is n
         return arr.length;
     }
+    public static List<Integer> disappearedNumber(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;  // index=value-1 because array is from 1 to n
+            if(arr[i]>0&&arr[i]<=arr.length&&arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+        List<Integer> ans=new ArrayList<>();
+        for(int j=0;j<arr.length;j++){
+          if(arr[j]!=j+1){
+              ans.add(j+1);
+          }
+        }
+      return ans;
+    }
     public static void main(String[] args) {
-        int[] arr={3,2,1,0,5};
-        System.out.println(missingNumber(arr));
+        int[] arr={3,2,1,5,1,6,8,2};
+      //  System.out.println(missingNumber(arr));
+        System.out.println(disappearedNumber(arr));
+
     }
 }
