e4313bc68edf0dcc4893450bcf557a5fa75b0a32
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-03-23 19:29:04 +0545

    files added

---
+++ src/com/dsa/StringAndStringbuilder_12.java
@@ -0,0 +1,20 @@
+package com.dsa;
+
+public class StringAndStringbuilder_12 {
+    public static void main(String[] args) {
+        String name="hello";
+        // Creating different string class
+        String a=new String("hello");
+        String b=new String("hello");
+      //  System.out.println(name);
+        // String Comparison
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+
+        // charAt() functions
+        System.out.println(a.charAt(0));
+
+
+
+    }
+}
