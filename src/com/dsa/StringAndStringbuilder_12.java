8ed58e89f523ef09924c7b3d1ecaf9d5eb353a76
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-03-24 19:33:41 +0545

    Files added

--- src/com/dsa/StringAndStringbuilder_12.java
+++ src/com/dsa/StringAndStringbuilder_12.java
@@ -1,5 +1,7 @@
 package com.dsa;
 
+import java.util.ArrayList;
+
 public class StringAndStringbuilder_12 {
     public static void main(String[] args) {
         String name="hello";
@@ -15,12 +17,31 @@ public class StringAndStringbuilder_12 {
      //   System.out.println(a.charAt(0));
 
         // pretty printing in java
-        float c=123.4567f;
-        System.out.println("The value of c without formatting is : "+c);
-        System.out.printf("The formatted value of c is : %.2f \n",c);
+//        float c=123.4567f;
+//        System.out.println("The value of c without formatting is : "+c);
+//        System.out.printf("The formatted value of c is : %.2f \n",c);
+//
+//        System.out.println("The value of pie without formatting is :"+Math.PI);
+//        System.out.printf("The formatted value of pie is : %.2f",Math.PI);
 
-        System.out.println("The value of pie without formatting is :"+Math.PI);
-        System.out.printf("The formatted value of pie is : %.2f",Math.PI);
+        // String concatenation
+//        System.out.println('a'+'b'); // prints ascii value
+//        System.out.println("a"+"b");  // prints ab
+//        System.out.println("a"+'b');  // prints ab
+//        System.out.println("a"+1);    // prints a1
+//
+//        // complex data types
+//        System.out.println("a"+new ArrayList<>());  // prints a[] bcz arraylist is empty
+
+        // String performance
+//        String series="";
+//        for(int i=0;i<26;i++){
+//            char text=(char)('a'+i);
+//            series=series+text;
+//        }
+//        System.out.println(series);
+
+
 
 
 
