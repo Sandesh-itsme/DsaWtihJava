04dd4c79375de6b7dd13631a811e11388528a019
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-03-24 18:08:29 +0545

    files added

--- src/com/dsa/StringAndStringbuilder_12.java
+++ src/com/dsa/StringAndStringbuilder_12.java
@@ -8,11 +8,19 @@ public class StringAndStringbuilder_12 {
         String b=new String("hello");
       //  System.out.println(name);
         // String Comparison
-        System.out.println(a==b);
-        System.out.println(a.equals(b));
+   //     System.out.println(a==b);
+   //     System.out.println(a.equals(b));
 
         // charAt() functions
-        System.out.println(a.charAt(0));
+     //   System.out.println(a.charAt(0));
+
+        // pretty printing in java
+        float c=123.4567f;
+        System.out.println("The value of c without formatting is : "+c);
+        System.out.printf("The formatted value of c is : %.2f \n",c);
+
+        System.out.println("The value of pie without formatting is :"+Math.PI);
+        System.out.printf("The formatted value of pie is : %.2f",Math.PI);
 
 
 
