2a1ea3f209dacbc17a2b4a44dda5a533fa9515d7
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-03-25 19:38:59 +0545

    Files added

---
+++ src/com/dsa/Pattern_13.java
@@ -0,0 +1,51 @@
+package com.dsa;
+
+public class Pattern_13 {
+    public static void pattern1(int n){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern2(int n){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern3(int n){
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern4(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        // 1.
+     //  pattern1(4);
+
+        // 2.
+       // pattern2(4);
+
+        // 3.
+      //  pattern3(4);
+
+        // 4.
+        pattern4(4);
+
+
+    }
+}
