74566deb9d07bf02109645ef8ce891fb84e5af64
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-04-03 20:48:36 +0545

    Files added

--- src/com/dsa/Recursion_14.java
+++ src/com/dsa/Recursion_14.java
@@ -22,11 +22,27 @@ public class Recursion_14 {
         }
             return fibonacci(n-1)+fibonacci(n-2);
     }
+    public static int binarySearch(int[] arr,int target,int start,int end){
+        if(start>end){
+            // element is not found
+            return -1;
+        }
+        int mid=(start+end)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[mid]<target){
+           return binarySearch(arr,target,mid+1,end);
+        }
+        return binarySearch(arr,target,start,mid-1);
+    }
     public static void main(String[] args) {
       //  numbers(1);
-        for(int i=0;i<=5;i++){
-            System.out.print(fibonacci(i)+" ");
-        }
-
+      //  for(int i=0;i<=5;i++){
+      //      System.out.print(fibonacci(i)+" ");
+      //  }
+              int[] arr={1,2,3,4,5,6};
+              int target=3;
+        System.out.println(binarySearch(arr,target,0,arr.length-1));
     }
 }
