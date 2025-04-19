70a8bd3436a16dc031de9ccf4b8ed6894c3c22d8
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-04-19 18:21:11 +0545

    Files added

---
+++ src/com/dsa/BitwiseOperator_15.java
@@ -0,0 +1,25 @@
+package com.dsa;
+
+public class BitwiseOperator_15 {
+    public static void main(String[] args) {
+     // 1. Bitwise AND (&)
+        int a=6;
+        int b=7;
+        System.out.println(a&b);
+
+        // 2. Bitwise OR ()
+
+
+        // 3. XOR (Exclusive OR) (^)
+        System.out.println(a^b);
+
+        // 4. Complement (~)
+        System.out.println(~a);
+
+        // 5. Shift left(<<)
+        System.out.println(a<<1);   // shift left by 1 position
+
+        // 6. Shift right(>>)
+        System.out.println(a>>1);   // shift right by 1 position
+    }
+}
