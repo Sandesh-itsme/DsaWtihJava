4c022520849e04e772f63d6bd29a26a0ad9eceec
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-04-19 18:50:22 +0545

    Files added

---
+++ src/com/dsa/BitwiseQuestions_16.java
@@ -0,0 +1,23 @@
+package com.dsa;
+
+public class BitwiseQuestions_16 {
+    public static boolean isOdd(int n){
+        return (n&1)==1;
+    }
+    public static int singleRepetition(int[] arr){
+        int ans=0;
+        for(int i=0;i<arr.length-1;i++){
+            ans=arr[i]^arr[i+1];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        // 1. Odd or even
+        int n=6;
+        System.out.println(isOdd(n));
+
+        // 2. Number with single repetition in an array
+        int[] arr={1,2,3,4,3,2,1};
+        System.out.println(singleRepetition(arr));
+    }
+}
