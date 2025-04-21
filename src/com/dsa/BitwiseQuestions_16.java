ab7c9380d866a126c354de84b2534d25c1ab278a
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-04-21 19:10:23 +0545

    Files added

--- src/com/dsa/BitwiseQuestions_16.java
+++ src/com/dsa/BitwiseQuestions_16.java
@@ -11,20 +11,35 @@ public class BitwiseQuestions_16 {
         }
         return ans;
     }
+    public static int singleInAnArray(int[] arr){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         // 1. Odd or even
-        int n=6;
-        System.out.println(isOdd(n));
+     //   int n=6;
+    //    System.out.println(isOdd(n));
 
         // 2. Number with single repetition in an array
-        int[] arr={1,2,3,4,3,2,1};
-        System.out.println(singleRepetition(arr));
+      //  int[] arr={1,2,3,4,3,2,1};
+      // System.out.println(singleRepetition(arr));
 
         // 3. Single no. in an array
+     //   int[] arr1={1,-1,2,-2,3,-3,4};
+     //   System.out.println(singleInAnArray(arr1));
 
         // 4. Position of ith bit
+//        int num=1001101;
+//        int position=3;
+//        int ans=num&(1<<(position-1));
+//        System.out.println(ans);
+//        System.out.println(1<<(position-1));
 
         // 5. Set the ith bit to 1
+
 
         // 6.  Reset the ith bit to 0
 
