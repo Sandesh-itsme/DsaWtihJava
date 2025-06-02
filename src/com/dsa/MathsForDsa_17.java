30b2d1a8e9c3f8704cafdf7047c1a6f6e83a6864
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-06-02 12:32:42 +0545

    Files added

--- src/com/dsa/MathsForDsa_17.java
+++ src/com/dsa/MathsForDsa_17.java
@@ -28,15 +28,48 @@ public class MathsForDsa_17 {
            }
        }
     }
+    public static double squareRoot(int num){
+        int start=0;
+        int end=num;
+        double result=0.0;
+        int p=3;  // precision till we want our result
+        while(start<=end) {
+            int mid = (start + end) / 2;
+            if (mid * mid == num) {
+                return mid;
+            } else if (mid * mid < num) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+            // for decimal part
+            double increment = 0.1;
+            for (int i = 1; i <= p; i++) {
+                while (result * result < num) {
+                    result = result + increment;
+                }
+                // when loops break come back once
+                result = result - increment;
+                increment = increment / 10;  // for 2nd decimal place and 3rd and so on
+            }
+         }
+        return result;
+    }
     public static void main(String[] args) {
         // 1. Prime number
      //   int n=2;
      //   System.out.println(isPrime(n));
 
         // 2. Prime number from 2 to n
-        int n=40;
-        boolean[] prime=new boolean[n+1];
-        sieve(n,prime);
+      //  int n=40;
+      //  boolean[] prime=new boolean[n+1];
+      //  sieve(n,prime);
+
+        // 3. Square root of a number
+        // printing up to 3 decimal places
+        System.out.printf("%.3f",squareRoot(40));
+
 
 
 
