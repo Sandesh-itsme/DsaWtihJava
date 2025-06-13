0a6d6a21e113df0a8d326423349d485cd84cdd88
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-06-13 11:51:09 +0545

    Files added

--- src/com/dsa/RecursionArray_19.java
+++ src/com/dsa/RecursionArray_19.java
@@ -8,9 +8,23 @@ public class RecursionArray_19 {
         }
           return arr[index]<arr[index+1]&&sorted(arr,index+1);
     }
+    public static int linearSearch(int[] arr,int target,int index){
+        if(index==arr.length){
+            return -1;
+        }
+          if(arr[index]==target){
+              return index;
+          }else{
+                 return linearSearch(arr,target,index+1);
+          }
+    }
     public static void main(String[] args) {
+        //  1. Array is sorted or not
         int[] arr={1,2,3,4,5,6,7};
-        System.out.println(sorted(arr,0));
+     //   System.out.println(sorted(arr,0));
+
+        // 2. Linear search
+        System.out.println(linearSearch(arr,7,0));
 
 
     }
