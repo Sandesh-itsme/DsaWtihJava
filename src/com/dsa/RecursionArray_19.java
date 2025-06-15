17af61c684ccc2fd702ec61379041a3f574b3aa7
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-06-15 11:25:32 +0545

    Files added

--- src/com/dsa/RecursionArray_19.java
+++ src/com/dsa/RecursionArray_19.java
@@ -1,5 +1,7 @@
 package com.dsa;
 
+import java.util.ArrayList;
+
 public class RecursionArray_19 {
     public static boolean sorted(int[] arr,int index){
         // if it reaches last index
@@ -18,13 +20,26 @@ public class RecursionArray_19 {
                  return linearSearch(arr,target,index+1);
           }
     }
+    public static ArrayList returnList(int[] arr,int target,int index,ArrayList list){
+        if(index==arr.length){
+            return list;
+        }
+          if(target==arr[index]){
+              list.add(index);
+          }
+            return returnList(arr,target,index+1,list);
+    }
     public static void main(String[] args) {
         //  1. Array is sorted or not
-        int[] arr={1,2,3,4,5,6,7};
+        int[] arr={1,2,3,4,4,5,6,7};
      //   System.out.println(sorted(arr,0));
 
         // 2. Linear search
-        System.out.println(linearSearch(arr,7,0));
+       // System.out.println(linearSearch(arr,7,0));
+
+        // 3. Returning an array list
+        ArrayList<Integer>list=new ArrayList<>();
+        System.out.println(returnList(arr,4,0,list));
 
 
     }
