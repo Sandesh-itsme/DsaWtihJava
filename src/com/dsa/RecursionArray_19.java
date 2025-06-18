4af1e8fb93debd80f78750a73bdce8632bc1f672
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-06-18 19:28:53 +0545

    Files added

--- src/com/dsa/RecursionArray_19.java
+++ src/com/dsa/RecursionArray_19.java
@@ -20,7 +20,7 @@ public class RecursionArray_19 {
                  return linearSearch(arr,target,index+1);
           }
     }
-    public static ArrayList returnList(int[] arr,int target,int index,ArrayList list){
+    public static ArrayList<Integer> returnList(int[] arr,int target,int index,ArrayList list){
         if(index==arr.length){
             return list;
         }
@@ -29,6 +29,22 @@ public class RecursionArray_19 {
           }
             return returnList(arr,target,index+1,list);
     }
+      public static ArrayList<Integer> returnWithoutArgument(int[] arr,int target,int index){
+        ArrayList<Integer> list=new ArrayList<>();
+
+          if(index==arr.length){
+              return list;
+          }
+          // This will contain the answer from that function call only
+          if(target==arr[index]){
+              list.add(index);
+          }
+            // This contains answer that has been returned from the below function call
+
+          ArrayList<Integer> ansFromBelow=returnWithoutArgument(arr,target,index+1);
+           list.addAll(ansFromBelow);
+           return list;
+      }
     public static void main(String[] args) {
         //  1. Array is sorted or not
         int[] arr={1,2,3,4,4,5,6,7};
@@ -38,8 +54,11 @@ public class RecursionArray_19 {
        // System.out.println(linearSearch(arr,7,0));
 
         // 3. Returning an array list
-        ArrayList<Integer>list=new ArrayList<>();
-        System.out.println(returnList(arr,4,0,list));
+      //  ArrayList<Integer>list=new ArrayList<>();
+     //  System.out.println(returnList(arr,4,0,list));
+
+        // 4. Returning an array list without passing array list as an argument
+        System.out.println(returnWithoutArgument(arr,4,0));
 
 
     }
