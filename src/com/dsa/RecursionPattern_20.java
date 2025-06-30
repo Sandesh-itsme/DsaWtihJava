bd4e9a9e109ac60bfc64947457567cdac3544aa5
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-06-30 19:13:44 +0545

    Files added

--- src/com/dsa/RecursionPattern_20.java
+++ src/com/dsa/RecursionPattern_20.java
@@ -46,12 +46,33 @@ public class RecursionPattern_20 {
             }
 
         }
-
+      public static void selectionSort(int[] arr,int row,int col,int max){
+        if(row==0){
+            return;
+        }
+          if(col<row){
+              // for the first row(for the first pass)
+              if (arr[col] > arr[max]){
+                    // change the maximum element
+                  selectionSort(arr,row,col+1,col);
+              }else{
+                  selectionSort(arr,row,col+1,max);
+              }
+          }else{
+                  // first pass is completed now swapping the maximum element with last index
+              int temp=arr[max];
+              arr[max]=arr[row-1];
+              arr[row-1]=temp;
+              selectionSort(arr,row-1,0,0);
+          }
+      }
     public static void main(String[] args) {
       //  pattern(4,0);
       //  pattern2(4,0);
           int[] arr={1,3,2,5,4,7,6};
-        bubbleSort(arr,arr.length-1,0);
+     //   bubbleSort(arr,arr.length-1,0);
+     //   System.out.println(Arrays.toString(arr));
+        selectionSort(arr,arr.length,0,0);
         System.out.println(Arrays.toString(arr));
 
 
