205860bdbb4b0e3e34511a94090db1f9b5bec645
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-07-30 12:31:17 +0545

    Package files need to be completed

---
+++ src/package1/print.java
@@ -0,0 +1,8 @@
+package package1;
+import package2.greet;
+public class print {
+    public static void main(String[] args) {
+        // Calling a class from different package
+       //    greet1();
+    }
+}
---
+++ src/package2/greet.java
@@ -0,0 +1,10 @@
+package package2;
+
+public class greet {
+    public void greet1(){
+        System.out.println("Good Morning");
+    }
+    public static void main(String[] args) {
+
+    }
+}
