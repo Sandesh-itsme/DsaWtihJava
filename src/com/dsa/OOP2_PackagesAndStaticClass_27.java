8032456247be473e80a2eada90b6e78e52e0a968
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-07-31 12:17:08 +0545

    Static method completed

--- src/com/dsa/OOP2_PackagesAndStaticClass_27.java
+++ src/com/dsa/OOP2_PackagesAndStaticClass_27.java
@@ -2,6 +2,28 @@ package com.dsa;
 
 public class OOP2_PackagesAndStaticClass_27 {
     public static void main(String[] args) {
+        Car_27 c1 =new Car_27(100000,"Honda");
+        Car_27 c2 =new Car_27(200000,"Suzuki");
 
+        System.out.println(c1.price);
+        System.out.println(c1.name);
+        System.out.println(c2.price);
+        System.out.println(c2.name);
+
+        // accessing static variable
+
+        // c1.num
+        // c2.num   This is not good way to access static variable
+        System.out.println(Car_27.num+1);
+        System.out.println(Car_27.num+1);
+
+     //   greet();  This is non-static method.So,it cannot be accessed from static method ie.main
+        // We can create object of main to access non-static method
+        OOP2_PackagesAndStaticClass_27 obj=new OOP2_PackagesAndStaticClass_27();
+        obj.greet();
+
+    }
+    public void greet(){
+        System.out.println("I am non-static greet method.");
     }
 }
--- src/com/dsa/demo_27.java
+++
@@ -1,6 +0,0 @@
-package com.dsa;
-
-public class demo_27 {
-    Car_27 c1 =new Car_27(100000,"Honda");
-    Car_27 c2 =new Car_27(200000,"Suzuki");
-}
