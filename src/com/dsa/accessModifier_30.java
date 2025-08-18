432281a6c0f88b8a7d4726413e5bcee7c0a8c622
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-08-18 11:29:58 +0545

    Files on access modifier completely added.

--- src/com/dsa/accessModifier_30.java
+++ src/com/dsa/accessModifier_30.java
@@ -1,14 +1,28 @@
 package com.dsa;
 
 public class accessModifier_30 {
+   // accessModifier_30 a=new accessModifier_30();
+    int a;
+    private int b;
+    protected int c;
+    public int d;
+
+    // Using getter to access private member
+    public int getB(){
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
 
     public static void main(String[] args) {
-        Show s=new Show2();
+       // Show s=new Show2();
         // These all access modifier can be accessed from the same class
-        System.out.println(s.a);
-        System.out.println(s.b);
-        System.out.println(s.c);
-        System.out.println(s.d);
+//        System.out.println(s.a);
+//        System.out.println(s.b);
+//        System.out.println(s.c);
+//        System.out.println(s.d);
     }
 }
 
--- src/hello/access.java
+++ src/hello/access.java
@@ -1,7 +1,19 @@
 package hello;
 
 import com.dsa.Show2;
+import com.dsa.accessModifier_30;
 
-public class access extends Show2 {
+public class access extends accessModifier_30{
+    public static void main(String[] args) {
+        accessModifier_30 access1=new accessModifier_30();
+        access access2=new access();
 
+       //  int n1=access2.a;  Since a has default modifier so it cannot be accessed from different package.
+           int n2= access2.getB();  // Since b has private modifier so we use getter setter to access it.
+           int n3=access2.c;        // Since access is the subclass of accessModifier_30 so we can access
+                                    // protected member from different package also
+           int n4=access2.d;        // Since d has public modifier so it can be accessed from anywhere.
+
+
+    }
 }
