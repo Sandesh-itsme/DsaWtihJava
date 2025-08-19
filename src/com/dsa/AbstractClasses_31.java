6d6150552c46d70e637dd28c1282804ae2af5cdb
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-08-19 13:00:33 +0545

    Files on abstract class added

---
+++ src/com/dsa/AbstractClasses_31.java
@@ -0,0 +1,52 @@
+package com.dsa;
+
+public class AbstractClasses_31 {
+
+    public static void main(String[] args) {
+        // We cannot create object of abstract class
+      //  Country c=new Country();
+
+
+        Nepal n=new Nepal("Nepal");
+        n.name();
+        n.normal();
+
+        // Accessing static method of abstract class with class name
+        Country.print();
+
+        // Abstract class can be used as reference variable
+           Country object=new Nepal("Nepal");
+
+    }
+}
+abstract class Country{
+  String name;
+
+  public Country(String name){
+      this.name=name;
+  }
+  //  We cannot create abstract constructor
+ // abstract Country();
+
+   // We cannot create abstract static class
+  //  abstract static void hello();
+
+  abstract void name();
+  public void normal(){
+      System.out.println("I am normal method.");
+  }
+    public static void print(){
+        System.out.println("I am static method of abstract class.");
+    }
+}
+      class Nepal extends Country{
+          public Nepal(String name) {
+              super(name);
+          }
+          @Override
+          public void name(){
+              System.out.println("Nepal");
+          }
+
+      }
+
