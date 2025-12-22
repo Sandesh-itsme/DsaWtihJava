b4cc1481d3b03da773030730f27b829d19f40200
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-12-22 16:52:02 +0545

    Files updated

--- src/com/dsa/Singleton_27.java
+++ src/com/dsa/Singleton_27.java
@@ -1,8 +1,8 @@
 package com.dsa;
 
 public class Singleton_27 {
-    // Singleton classes can have only one object.All the reference variable will point to the same object
-    // We will access this from Car_27
+    // Singleton classes can have only one object.All the reference variable will point to the same object.
+    // We will access this from Car_27.
 
     public static class Singleton{
         private Singleton(){
