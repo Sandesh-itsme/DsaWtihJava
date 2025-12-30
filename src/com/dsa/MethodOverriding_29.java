ca217eb31d93d62557679bd95d502fabd831fc22
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-12-30 18:10:14 +0545

    Files added

--- src/com/dsa/MethodOverriding_29.java
+++ src/com/dsa/MethodOverriding_29.java
@@ -22,6 +22,10 @@ public class MethodOverriding_29 {
         Car2 c=new Manual();
         // ParentClass object = new ChildClass();
 
+        // Static method can be inherited but cannot be overridden.
+        // Final class cannot be inherited and final method cannot be overridden.
+        // Class declared as final has all method final on default.
+
         c.name();
         // There must method name in both Car2 and Manual class.
         // But the method of Manual class will run because the object of which reference variable is created
