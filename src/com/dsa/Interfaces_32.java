e8351ad6c6ffc4a928275cc2022344ffef3d3b4d
Sandesh-itsme <neupanesandesh620@gmail.com>
2026-01-08 18:07:42 +0545

    Interfaces added

---
+++ src/com/dsa/Interfaces_32.java
@@ -0,0 +1,36 @@
+package com.dsa;
+
+public class Interfaces_32 {
+    public static void main(String[] args) {
+    animal a1=new animal();
+    a1.loud();
+    a1.name();
+    a1.speed();
+
+    // It can implements two interfaces even both have same method because they don't have their body
+    // and class implementing them can have their own body.So,it doesn't matter which one it is overriding.
+
+    }
+}
+  interface sound{
+    void loud();
+    void name();
+  }
+   interface walk{
+    void speed();
+    void name();
+  }
+     class animal implements sound,walk{
+    @Override
+   public void loud(){
+        System.out.println("It has medium loudness.");
+    }
+    @Override
+    public void name(){
+        System.out.println("It's name is dog.");
+    }
+    @Override
+    public void speed(){
+        System.out.println("It has medium speed.");
+    }
+     }
